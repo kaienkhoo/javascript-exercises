17cfd8f58b910261fae67129a33f205d851a87c1
Kai <100989619+kaienkhoo@users.noreply.github.com>
2025-09-12 17:32:47 +0800

    Completed array exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...value) {
+   return arr.filter(val => !value.includes(val))
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+      if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+    if (start < 0 || end < 0) return "ERROR";
+    if (start > end) {
+        const temp = start;
+        start = end;
+        end = temp;
+    }
+    
+    let arr = []
+    for(let i = start; i<=end; i++) {
+        arr.push(i)
+    }
 
+    return arr.reduce((total,currentItem) => total + currentItem, 0)
 };
+
+console.log(sumAll(1,4))
 
 // Do not edit below this line
 module.exports = sumAll;
